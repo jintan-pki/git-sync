3e422ae8b03f7141ec2f4d081a78adb67b89d4ea
Jin Tan <jin.tan@perkinelmer.com>
2023-02-13 10:49:34 +0800

    Update git-sync.sh

--- git-sync.sh
+++ git-sync.sh
@@ -29,25 +29,25 @@ echo "SOURCE=$SOURCE_REPO:$SOURCE_BRANCH"
 echo "DESTINATION=$DESTINATION_REPO:$DESTINATION_BRANCH"
 
 git lfs install --skip-smudge
-# https://shuhrat.github.io/programming/git-lfs-tips-and-tricks.html
-git config --unset-all http."https://github.com/".extraheader || :
-echo "Current LFS"
-git config --list | grep "filter.lfs"
-echo "DISABLE LFS"
-git config --global filter.lfs.required true
-git config --global filter.lfs.clean "git-lfs clean -- %f"
-git config --global filter.lfs.smudge "git-lfs smudge --skip -- %f"
-git config --global filter.lfs.process "git-lfs filter-process --skip"
+## https://shuhrat.github.io/programming/git-lfs-tips-and-tricks.html
+#git config --unset-all http."https://github.com/".extraheader || :
+#echo "Current LFS"
+#git config --list | grep "filter.lfs"
+#echo "DISABLE LFS"
+#git config --global filter.lfs.required true
+#git config --global filter.lfs.clean "git-lfs clean -- %f"
+#git config --global filter.lfs.smudge "git-lfs smudge --skip -- %f"
+#git config --global filter.lfs.process "git-lfs filter-process --skip"
 
 
 if [[ -n "$SOURCE_SSH_PRIVATE_KEY" ]]; then
   # Clone using source ssh key if provided
-  GIT_LFS_SKIP_SMUDGE=1 git clone -c core.sshCommand="/usr/bin/ssh -i ~/.ssh/src_rsa" "$SOURCE_REPO" /root/source --origin source && cd /root/source
+  git clone -c core.sshCommand="/usr/bin/ssh -i ~/.ssh/src_rsa" "$SOURCE_REPO" /root/source --origin source && cd /root/source
 else
-  GIT_LFS_SKIP_SMUDGE=1 git clone "$SOURCE_REPO" /root/source --origin source && cd /root/source
+  git clone "$SOURCE_REPO" /root/source --origin source && cd /root/source
 fi
 
-git lfs install --skip-smudge
+#git lfs install --skip-smudge
 git remote add destination "$DESTINATION_REPO"
 #git lfs fetch --all
 
