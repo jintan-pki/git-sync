fc24777bc7002cb9fd9bfac7cc8d604a40a1322f
Jin Tan <34757537+jintan-pki@users.noreply.github.com>
2023-02-20 14:04:40 +0800

    Update git-sync.sh

--- git-sync.sh
+++ git-sync.sh
@@ -64,5 +64,4 @@ if [[ -n "$DESTINATION_SSH_PRIVATE_KEY" ]]; then
   git config --local core.sshCommand "/usr/bin/ssh -i ~/.ssh/dst_rsa"
 fi
 
-git lfs push --all origin main
-git push destination "${SOURCE_BRANCH}:${DESTINATION_BRANCH}" --progress --force
+git push destination "${SOURCE_BRANCH}:${DESTINATION_BRANCH}" --force --no-verify
